f53984feb5792c1cb4810e1bbddfd52ff36a32a3
Reginaldo Pedro de Lima <reginaldo.pedro.de.lima@gmail.com>
2021-01-09 10:45:08 -0300

    post /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,14 +9,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 //para registrar o componente infetavel, coloca a anotiation @Component, mas é meçllçhor @SErvice
 @Service
 public class OrderService {
 	@Autowired // injeção de dependências
 	private OrderRepository repository ;
+	
+	@Autowired
+	private ProductRepository productRepository;
 	
 	
 	//pra nã odar lock no banco
@@ -27,4 +35,19 @@ public class OrderService {
 		
 	}
 
+	@Transactional()
+	public OrderDTO insert(OrderDTO dto){
+		
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(),dto.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProduts().add(product);			
+		}
+		
+		order = repository.save(order);
+		return new OrderDTO(order);
+		
+	}
 }
